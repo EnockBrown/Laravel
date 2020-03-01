7aa217e2fd57b2302f2a8ac4d94770c01ff0e04a
ManEnock <enochcheruiyot876@gmail.com>
2020-03-01 15:26:10 +0300

    verification layout

--- app/src/main/java/com/example/laravel/Phone_verification.java
+++ app/src/main/java/com/example/laravel/Phone_verification.java
@@ -2,13 +2,26 @@ package com.example.laravel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Phone_verification extends AppCompatActivity {
 
+    Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_verification);
+
+        next=findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Phone_verification.this,Verify.class);
+                startActivity(i);
+            }
+        });
     }
 }
