dbd9d59cc318243f96be5000544e4d1e0f62cde1
Enock Cheruiyot <40239524+EnockBrown@users.noreply.github.com>
2021-01-30 23:33:23 +0300

    Update RetrofitClient.java

--- app/src/main/java/com/example/laravel/Api/RetrofitClient.java
+++ app/src/main/java/com/example/laravel/Api/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private  static final String BASE_URL="http://192.168.0.106:8000/api.restService/v1/";
+    private  static final String BASE_URL="http://ip address/api.restService/v1/";
     private static RetrofitClient mInstance;
     private Retrofit retrofit;
     private RetrofitClient(){
